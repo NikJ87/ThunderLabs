9d4fcfd0fb667034964c251c4cabf9f1b83255cc
NikJ87 <35980502+NikJ87@users.noreply.github.com>
2023-04-04 15:16:24 +1000

    responsive experience for text size on small devcies (#6)

--- src/components/intro-banner/index.tsx
+++ src/components/intro-banner/index.tsx
@@ -2,6 +2,7 @@ import styled from "@emotion/styled";
 import { Typography } from "@mui/material";
 import { useContent } from "../../hooks";
 import { AppleMusicIcon } from "../icons/apple-music";
+import { mediaMaxMd } from "../style.constants";
 
 const StyledContainer = styled.div`
   margin: 75px 0;
@@ -11,17 +12,29 @@ const StyledText = styled(Typography)`
   margin-bottom: 10px;
 `;
 
+const PrimaryHeading = styled(StyledText)`
+  ${mediaMaxMd} {
+    font-size: 1.5rem;
+  }
+`;
+
+const SubHeading = styled(StyledText)`
+  ${mediaMaxMd} {
+    font-size: 0.8rem;
+  }
+`;
+
 export const IntroBanner = () => {
   const content = useContent();
   return (
     <StyledContainer>
       <AppleMusicIcon />
-      <StyledText variant="h4" align="center">
+      <PrimaryHeading variant="h4" align="center">
         {content.intro.banner.primary}
-      </StyledText>
-      <StyledText variant="body1" align="center">
+      </PrimaryHeading>
+      <SubHeading variant="body1" align="center">
         {content.intro.banner.secondary}
-      </StyledText>
+      </SubHeading>
     </StyledContainer>
   );
 };
